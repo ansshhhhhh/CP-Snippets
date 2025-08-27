bd9f3e4b4a3d1554cbe0ee1a482f8fc6b0dafd3e
ansshhhhhh <22cs3016@rgipt.ac.in>
2025-08-27 11:57:52 +0000

    Added some more modular operations

--- ModularArithmetic.cpp
+++ ModularArithmetic.cpp
@@ -1,19 +1,34 @@
 #define int long long
 
-const int MOD = 1e9 + 7;
+const int mod = 1e9 + 7;
 
 
 int modpow ( int a , int b ) {
 	long long res = 1;
-	a %= MOD ;
+	a %= mod ;
 	while ( b > 0) {
-		if ( b & 1) res = ( res * a ) % MOD ;
-		a = ( a * a ) % MOD ;
-		b > >= 1;
+		if ( b & 1) res = ( res * a ) % mod ;
+		a = ( a * a ) % mod ;
+		b >>= 1;
 	}
 	return res ;
 }
 
 int modinv ( int q ) {
-	return modpow (q , MOD - 2) ;
+	return modpow (q , mod - 2) ;
+}
+
+int modmul(int a, int b){
+		a = a % mod;
+		b = b % mod;
+
+		return (a * b) % mod;
+}
+
+int moddiv(int a, int b){
+		return modmul(a, modinv(b));
+}
+
+int modsub(int a, int b){
+		return (a % mod - b % mod + mod) % mod;
 }
